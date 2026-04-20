3ea8769f0a06ae6d93df439309ff436d0ad6de26
nijo0006 <nijo0006@stud.ek.dk>
2026-04-20 23:15:29 +0200

    Init commit

--- java.js
+++ java.js
@@ -1,1 +1,23 @@
-console.log('Happy developing ✨')
+const sliders = document.querySelectorAll(".slider");
+
+sliders.forEach(slider => {
+    let slides = slider.querySelectorAll(".slide");
+    let current = 0;
+
+    slides[current].style.display = "block";
+
+    const nextBtn = slider.querySelector(".next");
+    const prevBtn = slider.querySelector(".prev");
+
+    nextBtn.addEventListener("click", () => {
+        slides[current].style.display = "none";
+        current = (current + 1) % slides.length;
+        slides[current].style.display = "block";
+    });
+
+    prevBtn.addEventListener("click", () => {
+        slides[current].style.display = "none";
+        current = (current - 1 + slides.length) % slides.length;
+        slides[current].style.display = "block";
+    });
+});
